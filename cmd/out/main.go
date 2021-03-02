b5c26b19b9150d245b2211c7ebb54030af5ae770
Matt Harrison <hi@matt-harrison.com>
2021-03-02 21:22:58 +0000

    add out back

---
+++ cmd/out/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/mtharrison/github-pr-sha-comment-resource/internal/resource"
+)
+
+func main() {
+	input, err := resource.GetInput(os.Stdin, true)
+	if err != nil {
+		log.Fatal("Could not read input: ", err)
+	}
+
+	sha, err := resource.GetShaFromDir(input.Params.Dir)
+	if err != nil {
+		log.Fatal("Could not get sha from dir: ", err)
+	}
+
+	pr, err := resource.GetPrNumberFromSha(input, sha)
+	if err != nil {
+		log.Println("Could not get PR from SHA: ", err)
+		return // no exit - could be a non-pr commit
+	}
+
+	comment := resource.FormatComment(input.Params.Comment)
+
+	url, err := resource.PostComment(input, comment, pr)
+	if err != nil {
+		log.Fatal("Could not post comment: ", err)
+	}
+
+	out, err := json.Marshal(resource.Output{Version: resource.Version{URL: url}})
+	if err != nil {
+		log.Fatal("Could not json marshal")
+	}
+
+	_, err = os.Stdout.Write(out)
+	if err != nil {
+		log.Fatal("Could not write to STDOUT")
+	}
+}
